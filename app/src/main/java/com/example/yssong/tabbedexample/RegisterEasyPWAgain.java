b27a536f1362d86dc4e1b3495d4a51e1100e02f8
YSSONG <thddudtn12@naver.com>
2017-06-29 17:08:35 +0900

    뒤로가기 초기화 추가

--- app/src/main/java/com/example/yssong/tabbedexample/RegisterEasyPWAgain.java
+++ app/src/main/java/com/example/yssong/tabbedexample/RegisterEasyPWAgain.java
@@ -22,6 +22,8 @@ public class RegisterEasyPWAgain extends AppCompatActivity {
                 Intent intent = new Intent(
                         getApplicationContext(), // 현재 화면의 제어권자
                         EasyPWOk.class); // 다음 넘어갈 클래스 지정
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK); //Back Stack 초기화
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);  // 현재화면이 RootTask가 됨
                 startActivity(intent); // 다음 화면으로 넘어간다
             }
         });
