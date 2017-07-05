8038054103324f2e3d63938a7f615ec606a6234c
YSSONG <thddudtn12@naver.com>
2017-07-05 10:41:23 +0900

    no message

--- app/src/main/java/com/example/yssong/tabbedexample/SeeDetail.java
+++ app/src/main/java/com/example/yssong/tabbedexample/SeeDetail.java
@@ -3,6 +3,7 @@ package com.example.yssong.tabbedexample;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,7 +11,7 @@ import android.widget.Button;
  * Created by YSSONG on 2017-06-07.
  */
 
-public class SeeDetail extends Activity {
+public class SeeDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bill_chat);
--- app/src/main/java/com/example/yssong/tabbedexample/Tab1Contacts.java
+++ app/src/main/java/com/example/yssong/tabbedexample/Tab1Contacts.java
@@ -1,7 +1,9 @@
 package com.example.yssong.tabbedexample;
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +15,7 @@ import android.widget.ListAdapter;
  * Created by YSSONG on 2017-04-16.
  */
 
-public class Tab1Contacts  extends Fragment{
+public class Tab1Contacts  extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
