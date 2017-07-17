7c0146367a2a65055f211be87f1ebfa9b3bcf622
LIMHJ\LimHJ <jumong40@naver.com>
2017-07-17 20:27:33 +0900

    push알림 추가

--- app/src/main/java/com/example/yssong/tabbedexample/MainActivity.java
+++ app/src/main/java/com/example/yssong/tabbedexample/MainActivity.java
@@ -1,31 +1,25 @@
 package com.example.yssong.tabbedexample;
 
 import android.content.Intent;
-import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-
 import android.widget.Button;
-import android.widget.TextView;
+
+import com.google.firebase.iid.FirebaseInstanceId;
 
 public class MainActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        FirebaseInstanceId.getInstance().getToken();
+
+        String token = FirebaseInstanceId.getInstance().getToken();
+        Log.d("FCM_Token", token);
+
         Button login = (Button) findViewById(R.id.button_Login);
         Button join = (Button) findViewById(R.id.button_Join);
 
